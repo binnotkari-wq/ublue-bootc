d879554f1be51417ee53ddc1393da7a39a02227c
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-08-04 12:59:20 +0200

    Auto-sync [dell-5485] : 2026-08-04 12:59:20

--- build_files/build.sh
+++ build_files/build.sh
@@ -15,13 +15,11 @@ dnf5 remove -y fedora-flathub-remote
 # flatpak remote-delete --system fedora --force || true
 
 
-### Suppression des logiciels rpm inutiles
+### Suppression de logiciels RPM non désirés
 # dépendances vérifiées sur un système installé :
 # rpm -qa | grep -iE 'firefox|gnome-tour|gnome-software|^qt|qgnomeplatform'
 # rpm -q --whatrequires gnome-software
 # rpm -q --whatrequires qt5-qtwayland qt6-qtwayland
-
-### Suppression de logiciels RPM non désirés
 dnf5 remove -y \
   firefox \
   firefox-langpacks \
@@ -31,6 +29,42 @@ dnf5 remove -y \
   qt5-qtbase \
   qt6-qtbase
 
+
+### Suppression des extensions GNOME Shell par défaut et de la session classique associée
+### rpm -qa | grep -iE 'gnome-shell-extension*'
+### ainsi que rpm -q --whatrequires pour chauqe paquet
+dnf5 remove -y \
+  gnome-classic-session \
+  gnome-shell-extension-apps-menu \
+  gnome-shell-extension-launch-new-instance \
+  gnome-shell-extension-places-menu \
+  gnome-shell-extension-window-list \
+  gnome-shell-extension-background-logo
+
+
+
+
+
+### Services système : désactivation classique
+systemctl disable \
+  NetworkManager-wait-online.service \
+  ModemManager.service
+
+### Services système : masquage (statique ou activation par socket/dbus)
+systemctl mask \
+  geoclue.service \
+  gssproxy.service \
+  sssd-kcm.service sssd-kcm.socket \
+  pcscd.service pcscd.socket
+
+### Services utilisateur (--global : pas de session active pendant le build)
+systemctl --global mask \
+  evolution-addressbook-factory.service \
+  evolution-calendar-factory.service \
+  evolution-alarm-notify.service \
+  evolution-source-registry.service \
+  org.gnome.SettingsDaemon.Smartcard.service \
+  org.gnome.SettingsDaemon.Wwan.service
 
 
 
